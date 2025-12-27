fff036e80fec73d1f9ebb5e221cd6baa9d72cffb
pandyamehul <pandya.mehul@live.com>
2025-12-27 14:16:52 +0100

    20251227#Implemented order service in BAL to process API request and provide response

--- eCommerce.OrderService/OrderService.BusinessLogicLayer/DTO/OrderUpdateRequest.cs
+++ eCommerce.OrderService/OrderService.BusinessLogicLayer/DTO/OrderUpdateRequest.cs
@@ -1,5 +1,5 @@
 ﻿namespace eCommerce.OrderService.BusinessLogicLayer.DTO;
-public record OrderUpdateRequest(Guid OrderID, Guid UserID, DateTime OrderDate, List<OrderItemAddRequest> OrderItems)
+public record OrderUpdateRequest(Guid OrderID, Guid UserID, DateTime OrderDate, List<OrderItemUpdateRequest> OrderItems)
 {
     public OrderUpdateRequest() : this(default, default, default, default)
     {
