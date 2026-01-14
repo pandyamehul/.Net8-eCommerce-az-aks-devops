79b4ddeaeb7d6d0651abf9de5894f95c8e9e72cc
pandyamehul <pandya.mehul@live.com>
2026-01-14 16:10:54 +0100

    20260114#Ocelot GW configuration and local env. unit testing of routes mapping

--- eCommerce.OrderService/OrderService.ApiGw/Program.cs
+++ eCommerce.OrderService/OrderService.ApiGw/Program.cs
@@ -3,7 +3,14 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+// Ocelot Configuration
+// Load File based on environment variable in ASPNETCORE_ENVIRONMENT
+
+builder.Configuration.AddJsonFile(
+    builder.Configuration["OCELOT_CONFIGFILE"] ?? "ocelot.json",
+    optional: false,
+    reloadOnChange: true
+);
 builder.Services.AddOcelot();
 
 var app = builder.Build();
