33c65a71a4d818b05aa4594d90c39442917b5018
pandyamehul <pandya.mehul@live.com>
2026-01-18 23:54:40 +0100

    20260118#Consumer update in Order service

--- eCommerce.OrderService/OrderService.BusinessLogicLayer/HttpClients/ProductServiceClient.cs
+++ eCommerce.OrderService/OrderService.BusinessLogicLayer/HttpClients/ProductServiceClient.cs
@@ -40,7 +40,7 @@ public class ProductServiceClient
                 return productFromCache;
             }
 
-            HttpResponseMessage response = await _httpClient.GetAsync($"/api/products/search/product-id/{productID}");
+            HttpResponseMessage response = await _httpClient.GetAsync($"/gateway/products/search/product-id/{productID}");
 
             if (!response.IsSuccessStatusCode)
             {
